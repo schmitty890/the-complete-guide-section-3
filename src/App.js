e53c864e25a09b338771ea275d26e4f4928ad24b
jschmitt <schmitty890@gmail.com>
2019-05-04 18:46:26 -0400

    adding a state object to hold state of the persons

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [{
+      name: 'Jason',
+      age: 26
+    },{
+      name: 'Bill',
+      age: 60
+    },{
+      name: 'Joe',
+      age: 34
+    }]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, i'm a component</h1>
-        <Person name="Jason" age="26"/>
-        <Person name="Bill" age="60">I never thought losing my mind would be so hard.</Person>
-        <Person name="Joe" age="34"/>
+        <button>switch</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I never thought losing my mind would be so hard.</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
