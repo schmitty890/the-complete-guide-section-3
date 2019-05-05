59af5983b4161bbf8debab398b21bed34995d6df
jschmitt <schmitty890@gmail.com>
2019-05-05 13:38:45 -0400

    manipulating state with setState method

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   state = {
     persons: [{
       name: 'Jason',
-      age: 26
+      age: 29
     },{
       name: 'Bill',
       age: 60
@@ -17,11 +17,26 @@ class App extends Component {
     }]
   }
 
+  switchNameHandler = () => {
+    console.log('clicked');
+    // this.state.persons[0].name = 'Jayyyson'; // this will not work as you are updating state directly
+    this.setState({ persons: [ {
+      name: 'Jayyyyyson',
+      age: 30
+    },{
+      name: 'Bill',
+      age: 60
+    },{
+      name: 'Joe',
+      age: 34
+    } ] })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, i'm a component</h1>
-        <button>switch</button>
+        <button onClick={this.switchNameHandler}>switch name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I never thought losing my mind would be so hard.</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
