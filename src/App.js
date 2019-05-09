f90b875f7b999c4d902e190911e6b05a061efc26
jschmitt <schmitty890@gmail.com>
2019-05-08 20:11:40 -0400

    setting classes dynamically

--- src/App.js
+++ src/App.js
@@ -54,10 +54,12 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
-      padding: '8px'
+      padding: '8px',
+      cursor: 'pointer'
     };
 
     let persons = null;
@@ -75,6 +77,7 @@ class App extends Component {
           })}
         </div>        
       );
+      style.backgroundColor = 'red';
     }
 
     return (
