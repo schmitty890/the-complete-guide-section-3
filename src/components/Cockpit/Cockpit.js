69bfdd780eb03b1ae79d19c491a671dc87b11ad5
jschmitt <schmitty890@gmail.com>
2019-05-09 15:30:12 -0400

    passing title prop from index.js to app.js and to cockpit.js

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -21,7 +21,7 @@ const cockpit = (props) => {
 
   return (
     <div className={classes.Cockpit}>
-      <h1>Hi, i'm a component</h1>
+      <h1>{props.title}</h1>
       <p className={assignedClasses.join(' ')}>This is working!</p>
       <button 
         className={btnClass}
